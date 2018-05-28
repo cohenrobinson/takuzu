4d55c896e7835b2fd20048834924741ba2d4a2d9
Cohen Robinson <Cohen@Cohens-MBP.lan>
2018-05-29 09:49:49 +1000

    updates to common

--- common.py
+++ common.py
@@ -7,9 +7,15 @@ class Takuzu_Square(object):
     """docstring for Takuzu_Square."""
     def __init__(self, takuzu_list):
         super(Takuzu_Square, self).__init__()
+        # rows of board
         self.rows = takuzu_list
+        # length of board
         self.length = len(takuzu_list)
-        self.columns = [takuzu_list[i][j] for i in range(self.length) for j in range(self.length)]
-
-xzy = Takuzu_Square([[1,2], [3, 4]])
-print(xzy.rows)
+        columns = []
+        for i in range(self.length):
+            column = []
+            for j in range(self.length):
+                column.append(takuzu_list[j][i])
+            columns.append(column)
+        # columns of board
+        self.columns = columns
--- takuzu_valid.py
+++ takuzu_valid.py
@@ -4,3 +4,42 @@
 # edited:
 
 from collections import defaultdict as dd
+from common import *
+
+def valid_takuzu(takuzu_list):
+    """Determines if a given takuzu_list is valid (or not)."""
+    takuzu_list = Takuzu_Square(takuzu_list)
+    rows = takuzu_list.rows
+    columns = takuzu_list.columns
+    row_occur = dd(int)
+    column_occur = dd(int)
+
+    # calculates number of occurances in row and
+    print(rows)
+    for row in rows:
+        for square in row:
+            row_occur[square] += 1
+    print(columns)
+    for column in columns:
+        for square in column:
+            column_occur += 1
+
+    pass
+
+
+
+if __name__ =="__main__":
+
+    test_cases = [(True, [[1, 0, None, None],
+                          [1, None, 0, 1],
+                          [0, 1, 0, None],
+                          [None, 1, 1, 0]])]
+    n = 1
+    for test in test_cases:
+        result = valid_takuzu(test[1])
+        if result == test[0]:
+            outcome = "passed!"
+        else:
+            outcome = "failed!"
+        print("for test_case {} valid_takuzu {} \n".format(n, outcome))
+        print("The output was {}".format(result))
